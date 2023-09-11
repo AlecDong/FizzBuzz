c7bdbb9b3f7fe186d1f150130936902ff1d5ca09
AlecDong <101013007+AlecDong@users.noreply.github.com>
2023-09-11 09:44:47 -0400

    create class to solve multiples problem

---
+++ src/Multiples.java
@@ -0,0 +1,19 @@
+public class Multiples {
+    public static void main(String[] args) {
+//        Method solving analytically
+        int x = 1000 / 3;
+        int y = 1000 / 5;
+        int z = 1000 / 15;
+        System.out.println(x + y - z);
+//        Method using for loop and iterating every number through 1-1000
+//        int count = 0;
+//        for (int i = 1; i <= 1000; i++) {
+//            boolean div3 = i % 3 == 0;
+//            boolean div5 = i % 5 == 0;
+//            if (div3 || div5) {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+    }
+}
